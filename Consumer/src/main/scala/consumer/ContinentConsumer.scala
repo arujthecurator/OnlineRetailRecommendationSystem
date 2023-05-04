015197bdf7e27bdfabdfc470c17a60594d5f4e15
Mohith Kamanuru <kamanurumohith@gmail.com>
2023-05-03 20:00:59 -0400

    refactored code

--- Consumer/src/main/scala/consumer/ContinentConsumer.scala
+++ Consumer/src/main/scala/consumer/ContinentConsumer.scala
@@ -1,53 +1,55 @@
 package consumer
 
-import java.time.Duration
 import java.util.{Collections, Properties}
 import org.apache.kafka.clients.consumer.{ConsumerConfig, ConsumerRecord, KafkaConsumer}
-import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig, ProducerRecord}
 import org.apache.kafka.common.serialization.{StringDeserializer, StringSerializer}
-import org.json4s._
-import org.json4s.native.JsonMethods._
 
-import scala.collection.JavaConverters.asScalaIteratorConverter
 import scala.collection.JavaConverters._
-import scala.collection.mutable
-
-//case class Data(
-//                 InvoiceNo: String,
-//                 StockCode: String,
-//                 Description: String,
-//                 Quantity: Int,
-//                 InvoiceDate: String,
-//                 UnitPrice: Double,
-//                 CustomerID: String,
-//                 Country: String
-//               )
 
 object ContinentConsumer extends App {
-  //def main(args: Array[String]): Unit = {
+
+  // Set properties for the consumer
   val props = new Properties()
   props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
   props.put(ConsumerConfig.GROUP_ID_CONFIG, "test-consumer-group")
   props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, classOf[StringDeserializer].getName)
   props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, classOf[StringDeserializer].getName)
 
+  // Create a KafkaConsumer instance
   val consumer = new KafkaConsumer[String, String](props)
 
+  // Subscribe to the specified topic
   consumer.subscribe(Collections.singletonList("unspecified-topic"))
 
-
+  // Start an infinite loop to poll records from Kafka
   while (true) {
-    val records = consumer.poll(1000)
-    for (record <- records.asScala) {
-      val invoice = record.value().split(",")(0).split(":")(1).replaceAll("\"", "")
-      val description = record.value().split(",")(2).split(":")(1).replaceAll("\"", "")
-      val country = record.value().split(",")(7).split(":")(1).replaceAll("\"", "").replaceAll("}", "")
-      println("Invoice: "+invoice+" Country: "+country)
+    val records = consumer.poll(1000) // Poll records from Kafka
+    for (record <- records.asScala) { // Iterate over records
+      val data = extractData(record.value()) // Extract data from record value
+      val invoice = data.InvoiceNo
+      val country = data.Country
+      println(s"Invoice: $invoice Country: $country") // Print extracted data
     }
-
-
   }
 
-  consumer.close()
-}
+  consumer.close() // Close the KafkaConsumer instance after processing all records
 
+  // Function to extract data from the record value
+  private def extractData(recordValue: String): Data = {
+    val parts = recordValue.split(",").map(_.split(":")(1).replaceAll("\"", ""))
+    Data(parts(0), parts(1), parts(2), parts(3).toInt, parts(4), parts(5).toDouble, parts(6), parts(7).replaceAll("}", ""))
+  }
+
+  // Class to hold the extracted data
+  case class Data(
+                   InvoiceNo: String,
+                   StockCode: String,
+                   Description: String,
+                   Quantity: Int,
+                   InvoiceDate: String,
+                   UnitPrice: Double,
+                   CustomerID: String,
+                   Country: String
+                 )
+
+}
--- Consumer/src/main/scala/consumer/DiscountConsumer.scala
+++ Consumer/src/main/scala/consumer/DiscountConsumer.scala
@@ -7,66 +7,92 @@ import org.apache.kafka.common.serialization.{StringDeserializer, StringSerializ
 import org.json4s._
 import org.json4s.native.JsonMethods._
 import org.json4s.native.Serialization
+import org.json4s.DefaultFormats
 
 import scala.collection.JavaConverters._
 
 
 case class AggregatedData(
-                 InvoiceNo: String,
-                 TotalAmount: Double,
-                 CustomerID: String,
-                 Country: String,
-                 StockCode: List[String]
-               )
+                           InvoiceNo: String,
+                           TotalAmount: Double,
+                           CustomerID: String,
+                           Country: String,
+                           StockCode: List[String]
+                         )
 
 object DiscountConsumer extends App {
+
+  // Set up Kafka consumer properties
   val props = new Properties()
   props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
   props.put(ConsumerConfig.GROUP_ID_CONFIG, "test-consumer-group")
   props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, classOf[StringDeserializer].getName)
   props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, classOf[StringDeserializer].getName)
 
+  // Create a Kafka consumer instance
   val consumer = new KafkaConsumer[String, String](props)
+
+  // Subscribe to the orders-topic topic
   consumer.subscribe(Collections.singletonList("orders-topic"))
 
+  // Initialize variables to hold state across records
   var lastInvoiceId: String = ""
-  var stokeCodeRecords: List[String] = List.empty[String]
+  var stockCodeRecords: List[String] = List.empty[String]
   var totalAmount: Double = 0
 
+  // Poll for records from the Kafka topic and process them
   while (true) {
     val records = consumer.poll(Duration.ofMillis(1000))
+
     for (record <- records.asScala) {
-      val invoice = record.value().split(",")(0).split(":")(1).replaceAll("\"", "")
-      val stockCode = record.value().split(",")(1).split(":")(1).replaceAll("\"", "")
-      val country = record.value().split(",")(7).split(":")(1).replaceAll("\"", "").replaceAll("}", "")
-      val CustomerID = record.value().split(",")(6).split(":")(1).replaceAll("\"", "")
-      val quantity:Integer = record.value().split(",")(3).split(":")(1).replaceAll("\"", "").toInt
-      val amount:Double = record.value().split(",")(5).split(":")(1).replaceAll("\"", "").toDouble
-      totalAmount = totalAmount + (quantity* amount)
-      if (invoice != lastInvoiceId && stokeCodeRecords.nonEmpty) {
+      // Extract data from the record
+      val invoice = getValueByKey(record.value(), "invoiceNo")
+      val stockCode = getValueByKey(record.value(), "stockCode")
+      val country = getValueByKey(record.value(), "country")
+      val customerID = getValueByKey(record.value(), "customerID")
+      val quantity = getValueByKey(record.value(), "quantity").toInt
+      val amount = getValueByKey(record.value(), "unitPrice").toDouble
+
+      // Aggregate data based on the invoice number
+      totalAmount = totalAmount + (quantity * amount)
+      if (invoice != lastInvoiceId && stockCodeRecords.nonEmpty) {
+        // Apply discount for orders with total amount over $100
         if(totalAmount > 100){
-          totalAmount = totalAmount - (totalAmount/10)  //discount
+          totalAmount = 0.9* totalAmount //discount
         }
+        // Create aggregated data object and serialize it to JSON
         val data = AggregatedData(
           lastInvoiceId,
           totalAmount,
-          CustomerID,
+          customerID,
           country,
-          stokeCodeRecords
+          stockCodeRecords
         )
         implicit val formats: Formats = DefaultFormats
         val jsonData = Serialization.write(data)
 
+        // Print the JSON data to the console - producer comes here
         println(jsonData)
-        stokeCodeRecords = List.empty[String]
+
+        // Reset state for the next invoice
+        stockCodeRecords = List.empty[String]
         totalAmount = 0
       }
 
-      stokeCodeRecords = stockCode :: stokeCodeRecords
+      // Update state with current record data
+      stockCodeRecords = stockCode :: stockCodeRecords
       lastInvoiceId = invoice
     }
-
   }
 
+  // Close the Kafka consumer when done
   consumer.close()
+
+  // Helper function to extract the value of a field from a JSON string
+  private def getValueByKey(jsonString: String, key: String): String = {
+    implicit val formats: Formats = DefaultFormats
+
+    val json = parse(jsonString)
+    (json \ key).extract[String]
+  }
 }
